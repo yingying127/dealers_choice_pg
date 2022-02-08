d784cfce1fe17e05a35f069da72f9b9787c6490e
Ying Ying <yingyingfengg@gmail.com>
2022-02-08 00:56:30 -0500

    added the two pathways

--- server.js
+++ server.js
@@ -2,24 +2,57 @@ const { client, syncAndSeed} = require('./db')
 
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
+
+app.use(morgan('dev'));
+
+app.use(express.static('public'));
 
 app.get('/', async(req, res, next) => {
     try {
         const response = await client.query('SELECT * FROM animal');
-        const species = response.rows;
+        const animals = response.rows;
         res.send(`
         <html>
         <head>
+            <link rel ="stylesheet" href="/style.css" />
         </head>
         <body>
             <h1>Four Fun Facts about Animals!</h1>
             <div>
+            <h2>Animal</h2>
             <h2>
-                ${
-                    species.map (animal => `
-                        <a href='/'>${animal.name}</a>
+                ${  
+                    animals.map (animal => `
+                        <a href="/animals/${animal.id}">${animal.name}</a>
                     `).join('')
                 }
+            </h2>
+            </div>
+        </body>
+        </html>
+        `)
+    }
+    catch (err) {
+        next(err);
+    } 
+})
+
+app.get('/animals/:id', async(req, res, next) => {
+    try {
+        const response = await client.query('SELECT * FROM animal WHERE id=$1', [req.params.id]);
+        const animals = response.rows[0];
+        res.send(`
+        <html>
+        <head>
+            <link rel ="stylesheet" href="/style.css" />
+        </head>
+        <body>
+            <h1>Four Fun Facts about Animals!</h1>
+            <div>
+            <h2><a href='/'>Animal</a> </h2>
+            <h2>
+                ${animals.name}
             </h2>
             </div>
         </body>
